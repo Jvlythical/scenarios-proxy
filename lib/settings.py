d2f6ddbc129cb41c9fc2c19d478133754368b95b
Michael Yen <Michael Yen>
2021-03-24 23:24:08 -0700

    Add support for settings hot reloading

--- lib/settings.py
+++ lib/settings.py
@@ -2,7 +2,14 @@ import os
 import yaml
 import pdb
 
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+from .logger import Logger
+
 class Settings:
+    LOG_ID = 'lib.settings'
+
     _instance = None
 
     def __init__(self):
@@ -12,11 +19,8 @@ class Settings:
             cwd = os.path.dirname(os.path.realpath(__file__))
             self.config_file_path = os.path.join(cwd, '..', 'config', 'settings.yml')
 
-            with open(self.config_file_path, 'r') as stream:
-                try:
-                    self.config = yaml.safe_load(stream)
-                except yaml.YAMLError as exc:
-                    pass
+            self.__load_config()
+            self.__observe_config()
 
     @classmethod
     def instance(cls):
@@ -59,3 +63,27 @@ class Settings:
             return None
 
         return mode.get(active_mode)
+
+    def reload_config(self, event):
+        Logger.instance().info(f"{self.LOG_ID}.reload_config")
+        self.__load_config()
+
+    def __load_config(self):
+        with open(self.config_file_path, 'r') as stream:
+            try:
+                self.config = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                pass
+
+    def __observe_config(self):
+        patterns = "*"
+        ignore_patterns = ""
+        ignore_directories = False
+        case_sensitive = True
+        event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+        event_handler.on_modified = self.reload_config
+
+        observer = Observer()
+        observer.schedule(event_handler, self.config_file_path)
+        observer.start()
