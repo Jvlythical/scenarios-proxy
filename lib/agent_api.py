f3e0fcdb37934b8760fdc6387f1bca96a891fdf8
Michael Yen <Michael Yen>
2021-04-10 16:40:53 -0700

    Add lib/agent_api.py

---
+++ lib/agent_api.py
@@ -0,0 +1,30 @@
+import base64
+import json
+import requests
+import pdb
+
+class AgentApi:
+    STATUSES_ENDPOINT = '/api/v1/admin/statuses'
+
+    def __init__(self, service_url):
+        self.service_url = service_url
+
+
+    def update_status(self, status_id, project_key):
+        project_data = self.decode_project_key(project_key)
+        url = f"{self.service_url}{self.STATUSES_ENDPOINT}/{status_id}"
+        return requests.put(url, data=str(project_data.get('id')))
+
+    @staticmethod
+    def decode_project_key(key):
+        # TODO: add specific error catching
+        try:
+            key = base64.b64decode(key)
+        except:
+            return {}
+
+        # TODO: add specific error catching
+        try:
+            return json.loads(key)
+        except:
+            return {}
